a0bf3a017a598f8fb9ff84036df8600496c5af92
h-shoya-1106 <shoya1023@ymail.ne.jp>
2025-07-14 16:34:24 +0900

    食材の登録処理実装

--- src/app/api/menu/create/route.ts
+++ src/app/api/menu/create/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../../../lib/prisma";
-import { parseISO } from "date-fns";
 
 export async function POST(req: NextRequest) {
     const body = await req.json();
@@ -19,8 +18,11 @@ export async function POST(req: NextRequest) {
         dishes: {
             name: string;
             dishStatusId: number;
-            quantityId: number;
-            amount: number | null;
+            materials: {
+                materialName: string;
+                quantityId: number;
+                amount: number | null;
+            }[];
         }[];
     } = body;
 
@@ -29,34 +31,54 @@ export async function POST(req: NextRequest) {
             data: {
                 userId: 1,
                 name: menuName,
-                date: parseISO(date),
+                date: new Date(date),
                 timeZoneId,
                 calorie,
                 menuDishes: {
-                    create: dishes.map((dish) => ({
-                        dish: {
-                            create: {
-                                name: dish.name,
-                                dishStatusId: dish.dishStatusId,
-                            },
-                        },
-                        quantity: {
-                            connect: {
-                                id: dish.quantityId,
-                            },
-                        },
-                        amount: dish.amount,
-                    })),
+                    create: await Promise.all(
+                        dishes.map(async (dish) => {
+                            return {
+                                dish: {
+                                    create: {
+                                        name: dish.name,
+                                        dishStatus: {
+                                            connect: {
+                                                id: dish.dishStatusId,
+                                            },
+                                        },
+                                        dishMaterials: {
+                                            create: await Promise.all(
+                                                dish.materials.map(async (material) => ({
+                                                    material: {
+                                                        connectOrCreate: {
+                                                            where: { displayName: material.materialName },
+                                                            create: { displayName: material.materialName },
+                                                        },
+                                                    },
+                                                    quantity: {
+                                                        connect: {
+                                                            id: material.quantityId,
+                                                        },
+                                                    },
+                                                    amount: material.amount,
+                                                }))
+                                            ),
+                                        },
+                                    },
+                                },
+                            };
+                        })
+                    ),
                 },
-            },
-            include: {
-                menuDishes: true,
             },
         });
 
         return NextResponse.json(menu);
     } catch (err) {
         console.error(err);
-        return NextResponse.json({ error: "登録に失敗しました" }, { status: 500 });
+        return NextResponse.json(
+            { error: "登録に失敗しました" },
+            { status: 500 }
+        );
     }
 }
